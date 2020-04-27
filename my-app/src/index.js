02d38e24efc208c0eec8f6cbd6311c278a5abcff
George Shilin <georgeshilin@MacBook-Air-George.local>
2020-04-27 16:01:38 +0300

    3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,38 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     value:null
-  //   }
-  // }
-  // state = {
-  //   value: null
-  // }
-
-
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={() => this.props.onClick()}
+    >
+      {/* {this.props.value} */}
+    </button>
+  );
 }
+
 
 class Board extends React.Component {
   state = {
-    squares: Array(9).fill(null)
+    squares: Array(9).fill(null),
+    xIsNext: true,
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X"
-    this.setState({ squares })
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ 
+      squares,
+      xIsNext: !this.state.xIsNext,
+     });
   }
   renderSquare(i) {
     return (
